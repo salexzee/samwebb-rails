adad915f32c70647d4ddd4040a846dd533014517
salexzee <samwebb@codingtestsite.com>
2015-04-14 13:43:48 +0930

    Add page titles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
 	def index
 	end
+
+	def portfolio
+	end
+
+	def contact
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
 
   root 'pages#index'
 
+  get 'pages/portfolio'
+  get 'pages/contact'
+
 end

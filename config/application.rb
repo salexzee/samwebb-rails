86f005afbf0693223daf7c12ac888addeb7ab007
salexzee <samwebb@codingtestsite.com>
2015-04-28 21:50:31 +0930

    fix attempt #2

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Samwebb
   class Application < Rails::Application
+    config.assets.enabled = true
+    config.assets.path << "#{Rails.root}/app/assets/fonts"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

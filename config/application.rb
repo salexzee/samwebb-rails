e59861302bf217a28bb97223b653d3a137abae2d
salexzee <samwebb@codingtestsite.com>
2015-05-22 21:20:56 +0930

    config

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Samwebb
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Serves assets
+    config.serve_static_assets = true
   end
 end

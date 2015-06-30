e966e2eed969bdbaf8b3865c1a7425f5d19754c0
salexzee <samwebb@codingtestsite.com>
2015-06-30 19:01:01 +0930

    Add ability to create posts

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,10 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    @article = Article.new(article_params)
+ 
+    @article.save
+    redirect_to @article
   end
 
   def edit
@@ -16,8 +20,15 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def destroy
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end

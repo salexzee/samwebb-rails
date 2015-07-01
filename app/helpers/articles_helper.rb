9bcad19947e6b28b091fdbcd02eeaf7242003cb2
salexzee <samwebb@codingtestsite.com>
2015-07-01 21:02:14 +0930

    Properly display each post

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -2,4 +2,8 @@ module ArticlesHelper
 	def cts_blogs
 		[["5 Amazing Websites", "http://codingtestsite.com/blogs/5-amazing-websites.php", "Nov 15, 2014"], ["Show and Hide using jQuery", "http://codingtestsite.com/blogs/hide-show-text.php", "Nov 1, 2014"]]
 	end
+
+	def format_time(datetime)
+		datetime.strftime("%b %d, %Y")
+	end
 end

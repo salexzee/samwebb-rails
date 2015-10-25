bfa1f90619f5bf3949ed413cdd54be4edfa2a1fd
salexzee <samwebb@codingtestsite.com>
2015-10-25 19:45:09 +0930

    can now sign in and out w/ user bar at the top

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :admins, :skip => :registrations
+
+  authenticate :admins do
+    resources :articles, only: [:new, :create, :edit, :update, :destroy]
+  end
+
   root 'articles#index'
 
   resources :articles
